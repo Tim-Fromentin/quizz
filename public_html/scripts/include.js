336c601e6c95fed6f48d0dd2970ad9531a85af87
Tim <tim.fromentin.dev.pro@gmail.com>
2024-09-07 18:49:55 +0200

    repare include

--- public_html/scripts/include.js
+++ public_html/scripts/include.js
@@ -1,27 +1,48 @@
-window.addEventListener('DOMContentLoaded', function() {
-    var navbar = document.getElementById('navbar');
-    var footer = document.getElementById('footer');
-  
-    var xhr = new XMLHttpRequest();
-    
-    xhr.open('GET', './components/navbar.html', true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            navbar.innerHTML = xhr.responseText;
-        }
-    };
-    xhr.send();
-  
-    // Code pour charger le contenu du footer
-    var xhrFooter = new XMLHttpRequest();
-    
-    xhrFooter.open('GET', './components/footer.html', true);
-    xhrFooter.onreadystatechange = function() {
-        if (xhrFooter.readyState === 4 && xhrFooter.status === 200) {
-            footer.innerHTML = xhrFooter.responseText;
-        }
-    };
-    xhrFooter.send();
+window.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.getElementById("navbar");
+  var footer = document.getElementById("footer");
 
+  // Code pour charger le contenu du navbar
 
-  });
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "./components/navbar.html", true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      navbar.innerHTML = xhr.responseText;
+    }
+    const btnHamb = document.querySelector('.btn_hamb')
+    const boxNavLinks = document.querySelector('.box_nav_links')
+
+    btnHamb.addEventListener('click', toggleAnimation);
+
+    function toggleAnimation() {
+        if (btnHamb.classList.contains('btn_hamb--active')) {
+            // Si la classe active est présente, on passe à l'état inverse
+            btnHamb.classList.remove('btn_hamb--active');
+            btnHamb.classList.add('btn_hamb--active-reverse');
+        } else {
+            // Si la classe inverse est présente, on repasse à l'état actif
+            btnHamb.classList.remove('btn_hamb--active-reverse');
+            btnHamb.classList.add('btn_hamb--active');
+        }
+    }
+
+    btnHamb.addEventListener('click', openNavLinksMobileAnimation)
+    function openNavLinksMobileAnimation(){
+        boxNavLinks.classList.toggle('box_nav_links--active')
+    }
+  };
+  xhr.send();
+
+  // Code pour charger le contenu du footer
+  var xhrFooter = new XMLHttpRequest();
+
+  xhrFooter.open("GET", "./components/footer.html", true);
+  xhrFooter.onreadystatechange = function () {
+    if (xhrFooter.readyState === 4 && xhrFooter.status === 200) {
+      footer.innerHTML = xhrFooter.responseText;
+    }
+  };
+  xhrFooter.send();
+});
