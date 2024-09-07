95559f18e61ffb443df75f4be1ad878c7dc063bf
Tim <tim.fromentin.dev.pro@gmail.com>
2024-09-07 18:40:53 +0200

    navbar

--- note.md
+++ note.md
@@ -1,3 +1,6 @@
 # note ici 
 ## extension a installer pour les devs:  
 - ANCHOR (ExodiusStudios.comment-anchors)
+
+# Liste asset utiliser 
+- roboto
--- public_html/scripts/include.js
+++ public_html/scripts/include.js
@@ -1,3 +1,6 @@
+
+
+
 window.addEventListener('DOMContentLoaded', function() {
     var navbar = document.getElementById('navbar');
     var footer = document.getElementById('footer');
@@ -9,7 +12,31 @@ window.addEventListener('DOMContentLoaded', function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             navbar.innerHTML = xhr.responseText;
         }
-    };
+        const btnHamb = document.querySelector('.btn_hamb')
+        const boxNavLinks = document.querySelector('.box_nav_links')
+
+        btnHamb.addEventListener('click', toggleAnimation);
+
+        function toggleAnimation() {
+            if (btnHamb.classList.contains('btn_hamb--active')) {
+                // Si la classe active est présente, on passe à l'état inverse
+                btnHamb.classList.remove('btn_hamb--active');
+                btnHamb.classList.add('btn_hamb--active-reverse');
+            } else {
+                // Si la classe inverse est présente, on repasse à l'état actif
+                btnHamb.classList.remove('btn_hamb--active-reverse');
+                btnHamb.classList.add('btn_hamb--active');
+            }
+        }
+
+        btnHamb.addEventListener('click', openNavLinksMobileAnimation)
+        function openNavLinksMobileAnimation(){
+            boxNavLinks.classList.toggle('box_nav_links--active')
+        }
+
+    }
+
+
     xhr.send();
   
     // Code pour charger le contenu du footer
